a80201598bd86827a1db8dc9bcfe7fa25c1c9b76
deft1991 <deft1991@gmail.com>
2017-12-09 15:03:15 +0300

    refactor

--- src/main/java/com/codewars/level8/EvenOrOdd.java
+++ src/main/java/com/codewars/level8/EvenOrOdd.java
@@ -4,7 +4,7 @@ package com.codewars.level8;
 *Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 */
 public class EvenOrOdd {
-    public static String even_or_odd(int number) {
+    private static String even_or_odd(int number) {
         //Place code here
         return number%2 == 0 ? "Even" : "Odd";
     }
