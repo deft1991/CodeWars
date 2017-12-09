86baee814f97e1c3eb12da5fd6f9d0658fb5c32b
deft1991 <deft1991@gmail.com>
2017-12-09 15:22:43 +0300

    Opposite Number

---
+++ src/main/java/com/codewars/level8/OppositeNumber.java
@@ -0,0 +1,12 @@
+package com.codewars.level8;
+
+import static java.lang.Math.abs;
+
+/*Very simple, given a number, find its opposite.*/
+ class OppositeNumber {
+     static int opposite(int number)
+    {
+//        return number < 0 ? abs(number) : -number;
+        return -number;
+    }
+}
---
+++ test/com/codewars/level8/OppositeNumberTest.java
@@ -0,0 +1,12 @@
+package com.codewars.level8;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OppositeNumberTest {
+    @Test
+    public void tests() {
+        assertEquals(-1, OppositeNumber.opposite(1));
+    }
+}
