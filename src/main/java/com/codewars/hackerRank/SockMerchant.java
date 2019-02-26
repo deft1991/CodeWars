ee792e6357bcf50f449c89012cc6fa8d4e7fcda1
Голицын Сергей Николаевич <sgolitsyn@phoenixit.ru>
2019-02-26 18:48:06 +0300

    add task in class

--- src/main/java/com/codewars/hackerRank/SockMerchant.java
+++ src/main/java/com/codewars/hackerRank/SockMerchant.java
@@ -9,7 +9,24 @@ import java.util.Scanner;
 
 /**
  * @author Golitsyn Sergey (sgolitsyn)
- * @since 2/26/2019
+ * @since 2/26/2019 John works at a clothing store. He has a large pile of socks that he must pair
+ *     by color for sale. Given an array of integers representing the color of each sock, determine
+ *     how many pairs of socks with matching colors there are.
+ *     <p>For example, there are socks with colors . There is one pair of color and one of color .
+ *     There are three odd socks left, one of each color. The number of pairs is .
+ *     <p>Function Description
+ *     <p>Complete the sockMerchant function in the editor below. It must return an integer
+ *     representing the number of matching pairs of socks that are available.
+ *     <p>sockMerchant has the following parameter(s):
+ *     <p>n: the number of socks in the pile ar: the colors of each sock Input Format
+ *     <p>The first line contains an integer , the number of socks represented in . The second line
+ *     contains space-separated integers describing the colors of the socks in the pile.
+ *     <p>Constraints
+ *     <p>where Output Format
+ *     <p>Return the total number of matching pairs of socks that John can sell.
+ *     <p>Sample Input
+ *     <p>9 10 20 20 10 10 30 50 10 20 Sample Output
+ *     <p>3
  */
 public class SockMerchant {
 
