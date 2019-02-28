284ecf865581ffe13812ca4536e7b06112733379
Голицын Сергей Николаевич <sgolitsyn@phoenixit.ru>
2019-02-28 14:01:05 +0300

    JavaOutputFormatting.java

---
+++ src/main/java/com/codewars/hackerRank/JavaOutputFormatting.java
@@ -0,0 +1,22 @@
+package com.codewars.hackerRank;
+
+import java.util.Scanner;
+
+/**
+ * @author Golitsyn Sergey (sgolitsyn)
+ * @since 2/28/2019
+ */
+public class JavaOutputFormatting {
+  public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("================================");
+    for(int i=0;i<3;i++)
+    {
+      String s1=sc.next();
+      int x=sc.nextInt();
+      System.out.printf("%-15s%03d%n", s1, x);
+    }
+    System.out.println("================================");
+
+  }
+}
