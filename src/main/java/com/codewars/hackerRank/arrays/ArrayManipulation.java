4037930ba812e6ebebf64e88a25309c92b7ea2dd
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-10-19 23:57:56 +0300

    added problem description

--- src/main/java/com/codewars/hackerRank/arrays/ArrayManipulation.java
+++ src/main/java/com/codewars/hackerRank/arrays/ArrayManipulation.java
@@ -5,6 +5,24 @@ import java.util.Scanner;
 
 /*
  * Created by sgolitsyn on 10/19/19
+ * Starting with a 1-indexed array of zeros and a list of operations,
+ * for each operation add a value to each of the array element between two given indices, inclusive.
+ * Once all operations have been performed, return the maximum value in your array.
+
+For example, the length of your array of zeros n = 10 . Your list of queries is as follows:
+
+    a b k
+    1 5 3
+    4 8 7
+    6 9 1
+Add the values of  k between the indices a and b inclusive:
+
+index->	 1 2 3  4  5 6 7 8 9 10
+	[0,0,0, 0, 0,0,0,0,0, 0]
+	[3,3,3, 3, 3,0,0,0,0, 0]
+	[3,3,3,10,10,7,7,7,0, 0]
+	[3,3,3,10,10,8,8,8,1, 0]
+The largest value is 10 after all operations are performed.
  */
 public class ArrayManipulation {
     // Complete the arrayManipulation function below.
@@ -36,7 +54,7 @@ public class ArrayManipulation {
             rez[from - 1] += count;
 
             if (to < n) {
-                rez[to] -=count;
+                rez[to] -= count;
             }
         }
 
