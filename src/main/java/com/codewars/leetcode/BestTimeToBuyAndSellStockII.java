c5e7bef618d5c7fc51699fadb7bb00c9aafed7ec
sgolitsyn <pt.sgolitsyn@prosper.com>
2019-11-27 20:48:11 +0300

    BestTimeToBuyAndSellStockII.java

---
+++ src/main/java/com/codewars/leetcode/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,30 @@
+package com.codewars.leetcode;
+
+/*
+ * Created by sgolitsyn on 11/26/19
+ */
+public class BestTimeToBuyAndSellStockII {
+    public static void main(String[] args) {
+        BestTimeToBuyAndSellStockII buyAndSellStockII = new BestTimeToBuyAndSellStockII();
+
+        System.out.println(buyAndSellStockII.maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(buyAndSellStockII.maxProfit(new int[]{1,2,3,4,5}));
+        System.out.println(buyAndSellStockII.maxProfit(new int[]{7,6,4,3,1}));
+    }
+
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+
+        if (prices.length <= 1){
+            return profit;
+        }
+
+        for (int i = 1; i < prices.length; i++) {
+            int diff = prices[i] - prices[i-1];
+            if (diff > 0){
+                profit +=diff;
+            }
+        }
+        return profit;
+    }
+}
