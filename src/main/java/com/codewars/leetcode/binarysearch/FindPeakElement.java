dfb06c5a7c7fdf9ad060db6e2cc1705a42949ece
sgolitsyn <pt.sgolitsyn@prosper.com>
2020-02-12 15:44:34 +0300

    FindPeakElement.java

---
+++ src/main/java/com/codewars/leetcode/binarysearch/FindPeakElement.java
@@ -0,0 +1,29 @@
+package com.codewars.leetcode.binarysearch;
+
+/*
+ * Created by sgolitsyn on 2/12/20
+ */
+public class FindPeakElement {
+
+    public static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findPeakElement(new int[]{1, 2}));
+        System.out.println(findPeakElement(new int[]{1, 2, 3, 1}));
+        System.out.println(findPeakElement(new int[]{1, 2, 1, 3, 5, 6, 4}));
+    }
+}
