f7c1857eebb4eaa59646f1e5643e4189e8e9f653
sgolitsyn <pt.sgolitsyn@prosper.com>
2020-05-19 16:06:22 +0300

    MoveZeroes and ABetterRepeatedDeletionAlgorithmIntro

---
+++ src/main/java/com/codewars/leetcode/array/ABetterRepeatedDeletionAlgorithmIntro.java
@@ -0,0 +1,7 @@
+package com.codewars.leetcode.array;
+
+/*
+ * Created by sgolitsyn on 5/19/20
+ */
+public class ABetterRepeatedDeletionAlgorithmIntro {
+}
--- src/main/java/com/codewars/leetcode/array/MoveZeroes.java
+++ src/main/java/com/codewars/leetcode/array/MoveZeroes.java
@@ -27,17 +27,12 @@ public class MoveZeroes {
 
     public void moveZeroes(int[] nums) {
 
+        int lastNonZero = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[i] == 0) {
-                    if (nums[j] != 0) {
-                        int tmp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = tmp;
-                        break;
-                    }
-                } else
-                    break;
+            if (nums[i] != 0) {
+                int tmp = nums[lastNonZero];
+                nums[lastNonZero++] = nums[i];
+                nums[i] = tmp;
             }
         }
     }
