f4267f379fe8a4f5276a94a81f6d9c3c497c76f7
sgolitsyn <pt.sgolitsyn@prosper.com>
2020-06-09 11:58:06 +0300

    GroupAnagrams

--- src/main/java/com/codewars/leetcode/handbook/GroupAnagrams.java
+++ src/main/java/com/codewars/leetcode/handbook/GroupAnagrams.java
@@ -8,6 +8,24 @@ import java.util.Map;
 
 /*
  * Created by sgolitsyn on 6/8/20
+ *
+ *
+ *
+ * Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter.
  */
 public class GroupAnagrams {
 
