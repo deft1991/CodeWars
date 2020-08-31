596469cecee798c929a2bb59eab2d9a9edc115b8
sgolitsyn <pt.sgolitsyn@prosper.com>
2020-08-31 13:32:09 +0300

    some problems =)

---
+++ src/main/java/com/codewars/leetcode/topInterviewQuestions/easy/PalindromeLinkedList.java
@@ -0,0 +1,31 @@
+package com.codewars.leetcode.topInterviewQuestions.easy;
+
+import com.codewars.leetcode.handbook.ListNode;
+
+/*
+ * Created by sgolitsyn on 8/28/20
+ *
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+Follow up:
+Could you do it in O(n) time and O(1) space?
+ */
+public class PalindromeLinkedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isPalindrome(ListNode head) {
+
+        return false;
+    }
+}
