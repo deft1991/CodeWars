309d6374d7a658cc1957e452890b308e4721a22d
sgolitsyn <pt.sgolitsyn@prosper.com>
2020-08-31 13:33:08 +0300

    some problems =)

---
+++ src/main/java/com/codewars/leetcode/handbook/Test.java
@@ -0,0 +1,13 @@
+package com.codewars.leetcode.handbook;
+
+import java.util.Arrays;
+
+/*
+ * Created by sgolitsyn on 6/8/20
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
