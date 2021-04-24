9a9f3a6bd46f07555f687d527b095c1e1487ba83
deft <deft1991@gmail.com>
2021-04-24 17:42:34 +0300

    Some problems

--- src/main/java/com/codewars/leetcode/problems/medium/MostStonesRemovedWithSameRowOrColumn.java
+++ src/main/java/com/codewars/leetcode/problems/medium/MostStonesRemovedWithSameRowOrColumn.java
@@ -1,5 +1,8 @@
 package com.codewars.leetcode.problems.medium;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Sergey Golitsyn
  * created on 08.02.2021
@@ -8,22 +11,30 @@ public class MostStonesRemovedWithSameRowOrColumn {
 
     public static void main(String[] args) {
         MostStonesRemovedWithSameRowOrColumn m = new MostStonesRemovedWithSameRowOrColumn();
-//        System.out.println(m.removeStones(new int[][]{{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}}));
-//        System.out.println(m.removeStones(new int[][]{{0,0},{0,2},{1,1},{2,0},{2,2}}));
-//        System.out.println(m.removeStones(new int[][]{{0,0}}));
+        System.out.println(m.removeStones(new int[][]{{0,0},{0,1},{1,0},{1,2},{2,1},{2,2}}));
+        System.out.println(m.removeStones(new int[][]{{0,0},{0,2},{1,1},{2,0},{2,2}}));
+        System.out.println(m.removeStones(new int[][]{{0,0}}));
         System.out.println(m.removeStones(new int[][]{{0, 1}, {1, 0}}));
-//        System.out.println(m.removeStones(new int[][]{{0, 1}, {1, 0}, {1, 1}}));
-//        System.out.println(m.removeStones(new int[][]{{3, 2}, {0, 0}, {3, 3}, {2, 1}, {2, 3}, {2, 2}, {0, 2}}));
+        System.out.println(m.removeStones(new int[][]{{0, 1}, {1, 0}, {1, 1}}));
+        System.out.println(m.removeStones(new int[][]{{3, 2}, {0, 0}, {3, 3}, {2, 1}, {2, 3}, {2, 2}, {0, 2}}));
     }
 
     public int removeStones(int[][] stones) {
-        int n = stones.length;
-        int m = stones[0].length;
+        int N = stones.length;
 
-        new DSU(n * m);
+        DSU dsu = new DSU(2000);
 
+        for (int[] stone : stones) {
+            if (!dsu.isConnected(stone[0], 1000 + stone[1])) {
+                dsu.union(stone[0], 1000 + stone[1]);
+            }
+        }
 
-        return 0;
+        Set<Integer> seen = new HashSet<>();
+        for (int[] stone: stones)
+            seen.add(dsu.root(stone[0]));
+
+        return N - seen.size();
     }
 
     class DSU {
@@ -35,13 +46,24 @@ public class MostStonesRemovedWithSameRowOrColumn {
                 parent[i] = i;
         }
 
-        public int find(int x) {
-            if (parent[x] != x) parent[x] = find(parent[x]);
-            return parent[x];
+        public boolean isConnected(int a, int b) {
+            return root(a) == root(b);
         }
 
-        public void union(int x, int y) {
-            parent[find(x)] = find(y);
+        public void union(int a, int b) {
+            int rootA = root(a);
+            int rootB = root(b);
+
+            parent[rootB] = rootA;
         }
+
+        private int root(int a) {
+            while (a != parent[a]) {
+                parent[a] = parent[parent[a]];
+                a = parent[a];
+            }
+            return a;
+        }
+
     }
 }
